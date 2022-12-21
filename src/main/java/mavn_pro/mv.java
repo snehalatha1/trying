6380a8770d8beca636dee18bd7263edc46253da5
snehalatha1 <snehalathaps15@gmail.com>
2022-12-21 12:46:30 +0530

    added src code

---
+++ src/main/java/mavn_pro/mv.java
@@ -0,0 +1,10 @@
+package mavn_pro;
+
+public class mv {
+
+	public static void main(String[] args) {
+		System.out.println("hello world, dis is sneha");
+
+	}
+
+}
---
+++ src/main/java/pac/cla.java
@@ -0,0 +1,10 @@
+package pac;
+
+public class cla {
+
+	public static void main(String[] args) {
+		System.out.println("integrating eclipse vth github");
+
+	}
+
+}
